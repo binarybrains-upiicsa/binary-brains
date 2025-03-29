278120e98eec8b59e02291d82fe36af9328c6955
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-03-29 00:19:30 -0600

    style: Formatted code

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://binarybrains-upiicsa.github.io/"
+  site: "https://binarybrains-upiicsa.github.io/",
 });
