29e8c30c76660abc4ab5b82363c384778873cf21
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-03-29 00:18:27 -0600

    chore: Setted up site and removed extra posts

--- src/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-export const DEFAULT_REPOSITORY = {
-  title: "Proximamente...",
-  link: "https://github.com/BinaryBrainsUPIICSA",
-  imagePath: "/images/repositories/github.jpg",
-  description:
-    "En Binary Brains, estamos trabajando duro en emocionantes proyectos que llegarán muy pronto.",
-};
